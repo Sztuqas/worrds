ec25123ed96e4bfcabf6dfac9bf88454031c55b9
Dawid Franczak <d.franczak13@gmail.com>
2024-05-03 14:36:35 +0200

    create html elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatIconModule} from "@angular/material/icon";
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Words';
 }
+
